6e2889dbfcedf5f56fb3908b46c32cb0bb212232
Paulo Cesar <109388731+paulocdeveloper@users.noreply.github.com>
2023-07-29 00:06:29 -0300

    responsive mobile

--- script.js
+++ script.js
@@ -31,17 +31,6 @@ sr.reveal('.content-projetos',{
     duration:2000
  })
 
-sr.reveal('.contato',{
-    rotate:{ x:100, y:10, z:0},
-    duration:2000
- })
 
-sr.reveal('.content-contato',{
-    rotate:{ x:100, y:10, z:0},
-    duration:2000
- })
 
-sr.reveal('.email',{
-    rotate:{ x:100, y:10, z:0},
-    duration:2000
- })
+
